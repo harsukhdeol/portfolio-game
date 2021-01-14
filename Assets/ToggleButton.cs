93fd33d7231eb730080f559091d0e32be8bd7186
Harsukh Deol <centaurgolden@gmail.com>
2021-01-13 21:22:02 -0500

    cleaned up files

--- Assets/ToggleButton.cs
+++ Assets/ToggleButton.cs
@@ -1,23 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ToggleButton : MonoBehaviour
 {
 public GameObject Button;
 public GameObject Canvas;
 
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnMouseOver() {
         Button.gameObject.SetActive(true);
@@ -29,6 +19,9 @@ public GameObject Canvas;
     }
 
     private void OnMouseDown() {
+        if(EventSystem.current.IsPointerOverGameObject())
+            return;
+        
         Canvas.gameObject.SetActive(true);
     }
 }
